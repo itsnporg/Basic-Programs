b0b339650f237f8fa3fc0596a9b9b92dfe4856ad
Rounak Agrawal <74702699+UserRounakk@users.noreply.github.com>
2022-10-01 11:21:34 +0530

    chessboard program (#54)
    create simple chessboard layout using # and space

---
+++ JavaScript/ChessBoard.js
@@ -0,0 +1,18 @@
+var bindingSize = 8;
+var printString = '';
+var isSpace = false;
+
+for (let i = 1; i <= bindingSize; i++) {
+    i % 2 == 0 ? isSpace = true : isSpace = false;
+    for (let k = 0; k < bindingSize; k++) {
+        if (isSpace) {
+            printString = printString + '#';
+            isSpace = false;
+        } else {
+            printString = printString + ' ';
+            isSpace = true;
+        }
+    }
+    printString = printString + '\n'
+}
+console.log(printString)
