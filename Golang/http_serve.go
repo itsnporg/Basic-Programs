27ac49b05d6bd4ae24553661c3babb59f8646c75
Asep saepulloh <asepulloh0109@gmail.com>
2022-10-01 09:24:34 +0700

    add golang script (#21)

---
+++ Golang/http_serve.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func handlerGreeting(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Endpoint Hit")
+	io.WriteString(w, "Hello World")
+}
+
+func main() {
+	fmt.Println("HTTP Served")
+	http.HandleFunc("/", handlerGreeting)
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
