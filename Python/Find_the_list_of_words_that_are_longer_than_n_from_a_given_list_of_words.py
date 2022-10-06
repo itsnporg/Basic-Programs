411ffd8cab1e3ddbbae8216f1ff6d7c1e429e1b5
yashkumarjha12 <72943344+yashkumarjha12@users.noreply.github.com>
2022-10-06 11:51:45 +0530

    Create Find_the_list_of_words_that_are_longer_than_n_from_a_given_list_of_words.py

---
+++ Python/Find_the_list_of_words_that_are_longer_than_n_from_a_given_list_of_words.py
@@ -0,0 +1,8 @@
+def long_words(n, str):
+    word_len = []
+    txt = str.split(" ")
+    for x in txt:
+        if len(x) > n:
+            word_len.append(x)
+    return word_len	
+print(long_words(3, "The quick brown fox jumps over the lazy dog"))
