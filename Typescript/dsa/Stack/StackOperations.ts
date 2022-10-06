ad4f2085e20351adb784f698cd5cc0da11acd848
Bimal Shrestha <bimalshrestha074@gmail.com>
2022-10-06 17:19:35 +0545

    comments added for stack operations

--- Typescript/dsa/Stack/StackOperations.ts
+++ Typescript/dsa/Stack/StackOperations.ts
@@ -11,10 +11,10 @@ interface Stack {
 //push operation on stack
 class Stack {
     items: any[];
-    top: number;
+    top: number; //top pointer
     constructor() {
-        this.items = [];
-        this.top = -1;
+        this.items = []; //initialize the stack
+        this.top = -1; //initialize the top pointer
     }
     push = push;
     pop = pop;
@@ -24,24 +24,24 @@ class Stack {
 }
 
 function push(data: any) {
-    this.items.push(data);
+    this.items.push(data); //adding the element in the top of  stack
     this.top++;
 }
 //pop operation on stack
 function pop() {
     if (this.top === -1) {
-        return "Stack is empty";
+        return "Stack is empty"; //if the stack is empty then we can't remove any element from it
     }
     this.top--;
     return this.items.pop();
 }
 //peek operation on stack
 function peek() {
-    return this.items[this.top];
+    return this.items[this.top]; //returning the top element of the stack
 }
 // Check if the stack is empty
 function isEmpty() {
-    return this.top === -1;
+    return this.top === -1; //if the top is -1 then the stack is empty
 }
 //print the Stack operation on stack
 function printStack() {
@@ -61,13 +61,13 @@ stack.push(30);
 stack.push(40);
 stack.push(50);
 //printing stack elements
-console.log(stack.printStack());
+console.log(stack.printStack()); //output: 10 20 30 40 50 (top to bottom)
 //peek operation
-console.log(stack.peek());
+console.log(stack.peek()); //output: 50 (top element of the stack)
 //pop operation
-console.log(stack.pop());
+console.log(stack.pop()); //output: 50 (removed top element of the stack)
 //printing stack elements
-console.log(stack.printStack());
+console.log(stack.printStack()); //output: 10 20 30 40 (after removing top element of the stack)
 //isEmpty operation
-console.log(stack.isEmpty());
+console.log(stack.isEmpty()); //output: false (stack is not empty)
 
